55dd9649b068a003aacf5e7f56bcc7f992389b96
zerosnacks <zerosnacks@protonmail.com>
2021-12-19 00:52:55 +0100

    limit user mint

--- src/Lifeform.sol
+++ src/Lifeform.sol
@@ -120,6 +120,7 @@ contract Lifeform is ERC721, NFTSVG, Auth, ReentrancyGuard {
     /// @notice Mint token to address
     /// @param to The address to mint to.
     function mint(address to) external payable whenUnpaused {
+        require(balanceOf[msg.sender] <= 2, "USER_LIMITED_TO_MINT_TWO");
         require(totalSupply + 1 <= maxSupply, "ALL_TOKENS_MINTED");
         require(isSaleActive, "SALE_NOT_ACTIVE");
         require(salePrice <= msg.value, "INSUFFICIENT_ETHER");
