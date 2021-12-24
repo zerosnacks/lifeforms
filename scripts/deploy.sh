cfd5c7a0eb054b2ba04a1a04669652f64751189c
zerosnacks <zerosnacks@protonmail.com>
2021-12-24 20:56:47 +0100

    allow storing up to 25 BCT tokens

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,7 @@ set -eo pipefail
 # MockBCTAddr=$(deploy MockBCT)
 # log "MockBCT deployed at:" $MockBCTAddr
 
-LifeformAddr=$(deploy Lifeform 5 10000000000000000 5000000000000000000 100 0x0c504eAf83941DB8DF4913EFDD35913EFBE67984)
+LifeformAddr=$(deploy Lifeform 5 10000000000000000 25000000000000000000 100 0x0c504eAf83941DB8DF4913EFDD35913EFBE67984)
 log "Lifeform deployed at:" $LifeformAddr
+
+5000000000000000000
